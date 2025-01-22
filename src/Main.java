a31c08808a55943880075dd30b46b2182297e662
Maximus301191 <maxalut301191@gmail.com>
2025-01-22 16:40:54 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        int amount = 100; // начальный баланс клиента
+        int plus = 1000; // сумма пополнения
+        int summa = amount + plus; // сумма с пополнением
+        int bonus; // количество бонусов
+        int itog; // =итоговая сумма с бонусом
+                if(summa > 1000) {
+                bonus = summa / 100;
+                itog = bonus + summa;
+                        }
+                else {
+                    bonus = 0;
+                    itog = bonus + summa;
+                }
 
+                System.out.println( bonus + " рублей количество бонусов");
+        System.out.println( itog + " рублей итоговый счет");
     }
 }
